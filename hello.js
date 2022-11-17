7ddc025bcb7c099a8462798d35cfe6d17f2a29d1
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2022-11-17 00:14:17 +0000

    Introduction to functions

---
+++ hello.js
@@ -0,0 +1,13 @@
+const sayHello = function () {
+  console.log("Hello, world");
+};
+
+sayHello();
+
+const sayHelloName = function (name) {
+  console.log("Hello, " + name);
+};
+
+sayHelloName("Caliban");
+sayHelloName("Miranda");
+sayHelloName("Ferdinand");
