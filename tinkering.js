b7f1ce10f69c8b497f8ff48ca7d27d6923b6c8ab
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2022-11-17 02:50:35 +0000

    Loopy Lighthouse 2

--- tinkering.js
+++ tinkering.js
@@ -6,3 +6,19 @@ const result = multiply(2, 4);
 
 console.log(result);
 console.log("four" / 5);
+
+const loopyLighthouse = (range, multiples, words) => {
+  for (let number = range[0]; number <= range[1]; number++) {
+    if (number % multiples[0] === 0 && number % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (number % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (number % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(number);
+    }
+  }
+};
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
