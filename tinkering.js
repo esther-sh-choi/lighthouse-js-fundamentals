7aa7643910fdbbcb4ec3d5c97bc7fc67083299d2
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2022-11-18 06:43:54 +0000

    Kata 6 Smart Parking

--- tinkering.js
+++ tinkering.js
@@ -203,3 +203,68 @@ console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
 console.log(urlEncode("blue is greener than purple for sure"));
 */
+
+const whereCanIPark = function (spots, vehicle) {
+  // Code here!
+  const isSpotAvailable = (spot, vehicle) => {
+    if (vehicle === "regular") {
+      return spot === "R";
+    } else if (vehicle === "small") {
+      return spot === "R" || spot === "S";
+    } else if (vehicle === "motorcycle") {
+      return spot === "R" || spot === "S" || spot === "M";
+    }
+  };
+
+  for (let row = 0; row < spots.length; row++) {
+    for (let column = 0; column < spots[row].length; column++) {
+      if (isSpotAvailable(spots[row][column], vehicle)) {
+        return [column, row];
+      }
+    }
+  }
+
+  return false;
+};
+
+console.log(
+  whereCanIPark(
+    [
+      // COLUMNS ARE X
+      // 0    1    2    3    4    5
+      ["s", "s", "s", "S", "R", "M"], // 0 ROWS ARE Y
+      ["s", "M", "s", "S", "r", "M"], // 1
+      ["s", "M", "s", "S", "r", "m"], // 2
+      ["S", "r", "s", "m", "r", "M"], // 3
+      ["S", "r", "s", "m", "r", "M"], // 4
+      ["S", "r", "S", "M", "M", "S"], // 5
+    ],
+    "regular"
+  )
+);
+
+console.log(
+  whereCanIPark(
+    [
+      ["M", "M", "M", "M"],
+      ["M", "s", "M", "M"],
+      ["M", "M", "M", "M"],
+      ["M", "M", "r", "M"],
+    ],
+    "small"
+  )
+);
+
+console.log(
+  whereCanIPark(
+    [
+      ["s", "s", "s", "s", "s", "s"],
+      ["s", "m", "s", "S", "r", "s"],
+      ["s", "m", "s", "S", "r", "s"],
+      ["S", "r", "s", "m", "r", "s"],
+      ["S", "r", "s", "m", "R", "s"],
+      ["S", "r", "S", "M", "m", "S"],
+    ],
+    "motorcycle"
+  )
+);
