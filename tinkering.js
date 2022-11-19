a52912b4c7caef0f70e430abe7de9190b98d3c1f
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2022-11-19 00:42:51 +0000

    Kata 12 The Great Codeville Bake-off

--- tinkering.js
+++ tinkering.js
@@ -387,7 +387,7 @@ console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
 console.log(multiplicationTable(10));
 */
-
+/*
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
@@ -441,3 +441,55 @@ const cone = {
 const duck = [largeSphere, smallSphere, cone];
 
 console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
+*/
+
+const chooseRecipe = function (bakeryA, bakeryB, recipes) {
+  for (let recipe of recipes) {
+    if (
+      (bakeryA.includes(recipe.ingredients[0]) ||
+        bakeryB.includes(recipe.ingredients[0])) &&
+      (bakeryA.includes(recipe.ingredients[1]) ||
+        bakeryB.includes(recipe.ingredients[1]))
+    ) {
+      return recipe.name;
+    }
+  }
+};
+
+let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
+let bakeryB = ["milk", "butter", "cream cheese"];
+let recipes = [
+  {
+    name: "Coconut Sponge Cake",
+    ingredients: ["coconut", "cake base"],
+  },
+  {
+    name: "Persian Cheesecake",
+    ingredients: ["saffron", "cream cheese"],
+  },
+  {
+    name: "Custard Surprise",
+    ingredients: ["custard", "ground beef"],
+  },
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ["potatoes", "bay leaf", "raisins"];
+bakeryB = ["red bean", "dijon mustard", "apples"];
+recipes = [
+  {
+    name: "Potato Ganache",
+    ingredients: ["potatoes", "chocolate"],
+  },
+  {
+    name: "Sweet Fish",
+    ingredients: ["anchovies", "honey"],
+  },
+  {
+    name: "Nima's Famous Dijon Raisins",
+    ingredients: ["dijon mustard", "raisins"],
+  },
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
